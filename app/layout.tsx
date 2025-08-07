36ed5ac59115ba17066dd2745cc5e050a7273fd4
Vinoth <vinoth.youngglobes@gmail.com>
2025-08-07 10:11:33 +0000

    App Base

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+import Link from 'next/link';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>
+        <nav>
+          {/* Prefetched when the link is hovered or enters the viewport */}
+          <Link href="/blog">Blog</Link>
+          {/* No prefetching */}
+          <a href="/contact">Contact</a>
+        </nav>
+        {children}
+      </body>
+    </html>
+  );
+}
